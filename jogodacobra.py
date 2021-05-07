d24af2131f2c81a1d88552f4d5ecaddeb8332b4f
JoaquimBreno <winz993@gmail.com>
2021-05-07 00:37:33 -0300

    Game Mods

--- jogodacobra.py
+++ jogodacobra.py
@@ -2,6 +2,7 @@ import pygame
 import random
 from enum import Enum
 from collections import namedtuple
+import numpy as np
 
 
 pygame.init()
@@ -33,7 +34,7 @@ VERMELHO = (255,0,0)
 class JogoDaCobra:
 
     def __init__(self, largura = 640, altura = 480):
-        self.velocidade = 10
+        self.velocidade = 15
         self.temp_velocidade = 0
         self.d = 0
         self.largura = largura
@@ -42,8 +43,10 @@ class JogoDaCobra:
         self.display = pygame.display.set_mode((self.largura, self.altura))
         pygame.display.set_caption('Cobra')
         self.clock = pygame.time.Clock()
+        self.resetar()
 
         # inicia o estado do jogo
+    def resetar(self):
         self.direcao = Direcao.RIGHT
 
         self.cabeca = Point(self.largura/2, self.altura/2)
@@ -53,6 +56,7 @@ class JogoDaCobra:
         self.pontuacao = 0
         self.maca = None
         self.set_maca()
+        self.frame_interation
 
     def set_maca(self):
         x = random.randint(0, (self.largura-TAM_CAIXA)//TAM_CAIXA)*TAM_CAIXA
@@ -62,33 +66,29 @@ class JogoDaCobra:
         if self.maca in self.cobra:
             self.set_maca()
 
-    def playloop(self):
+    def playloop(self, acao):
+        self.frame_interation += 1
         # Coletar entradas
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    self.direcao = Direcao.LEFT
-                elif event.key == pygame.K_RIGHT:
-                    self.direcao = Direcao.RIGHT
-                elif event.key == pygame.K_UP:
-                    self.direcao = Direcao.UP
-                elif event.key == pygame.K_DOWN:
-                    self.direcao = Direcao.DOWN
+    
         # Movimentos
-        self.move(self.direcao)
+        self.move(acao)
         self.cobra.insert(0, self.cabeca)
         # Checa game over
+        recompensa = 0
         game_over = False
-        if self.colisao():
+        if self.colisao() or self.frame_interation > 100*len(self.snake):
             game_over = True
-            return game_over, self.pontuacao
+            recompensa = -10
+            return recompensa, game_over, self.pontuacao
             
         # Adiciona maca ou movimenta
         if self.cabeca == self.maca:
             self.pontuacao += 1
+            reward = 10
             # A cada 10 pontos adiciona uma velocidade
             if (self.pontuacao-self.temp_velocidade) == 10:
                 self.temp_velocidade = self.pontuacao
@@ -102,43 +102,60 @@ class JogoDaCobra:
         self.clock.tick(self.velocidade)
 
         # Retorna game over e pontuacao
-        return game_over, self.pontuacao
+        return reward, game_over, self.pontuacao
 
-    def colisao(self):
-        if self.cabeca in self.cobra[1:]:
+    def colisao(self, cb = None):
+        if cb is None:
+            cb = self.cabeca
+        if self.cb in self.cobra[1:]:
             return True        
-        elif self.cabeca.x > self.largura - TAM_CAIXA:
-            self.cabeca = Point(-20, self.cabeca.y)   
-        elif self.cabeca.x < 0:
-            self.cabeca = Point(640, self.cabeca.y)    
-        elif self.cabeca.y >  self.altura - TAM_CAIXA:
-            self.cabeca = Point(self.cabeca.x, -20)
-        elif self.cabeca.y <  0:
-            self.cabeca = Point(self.cabeca.x, 480)
+        elif self.cb.x > self.largura - TAM_CAIXA:
+            self.cb = Point(-20, self.cb.y)   
+        elif self.cb.x < 0:
+            self.cb = Point(640, self.cb.y)    
+        elif self.cb.y >  self.altura - TAM_CAIXA:
+            self.cb = Point(self.cb.x, -20)
+        elif self.cb.y <  0:
+            self.cb = Point(self.cb.x, 480)
         return False
 
     def atualiza_ui(self):
         self.display.fill(BLACK)
 
-        for pt in self.cobra:
-            pygame.draw.rect(self.display, VERDE, pygame.Rect(pt.x, pt.y, TAM_CAIXA, TAM_CAIXA))
+        for cb in self.cobra:
+            pygame.draw.rect(self.display, VERDE, pygame.Rect(cb.x, cb.y, TAM_CAIXA, TAM_CAIXA))
         pygame.draw.rect(self.display, VERMELHO, pygame.Rect(self.maca.x, self.maca.y, TAM_CAIXA, TAM_CAIXA))
 
         text = font.render("Pontuação: " + str(self.pontuacao), True, BRANCO)
         self.display.blit(text, [0, 0])
         pygame.display.flip()
 
-    def move(self, direcao):
+    def move(self, acao):
+
+        sent_horario = [Direcao.RIGHT,Direcao.DOWN, Direcao.LEFT, Direcao.UP]
+        idx = sent_horario.index(self.direcao)
+
+        if np.array_equal(acao, [1,0,0]):
+            nova_dir = clock_wise[idx]
+        if np.array_equal(acao, [0,1,0]):
+            next_idx = (idx + 1) % 4
+            nova_dir = sent_horario[next_idx]
+        else:
+            next_idx = (idx - 1) % 4
+            nova_dir = sent_horario[next_idx]
+        
+        self.direcao = new_dir
+
         x = self.cabeca.x
         y = self.cabeca.y
         # Verifica tecla e movimento anterior
-        if direcao == Direcao.RIGHT and self.d!=2:
+        if self.direcao == Direcao.RIGHT and self.d!=2:
             self.d=1
-        elif direcao == Direcao.LEFT and self.d!=1:
+        elif self.direcao == Direcao.LEFT and self.d!=1:
             self.d=2
-        elif direcao == Direcao.DOWN and self.d!=4:
+        elif self.direcao == Direcao.DOWN and self.d!=4:
             self.d=3
-        elif direcao == Direcao.UP and self.d!=3:
+        elif self.direcao == Direcao.UP and self.d!=3:
             self.d=4
 
         #Executa movimento
@@ -152,13 +169,3 @@ class JogoDaCobra:
             y-= TAM_CAIXA
 
         self.cabeca = Point(x,y)
-
-if __name__ == '__main__':
-
-    game = JogoDaCobra()
-
-    while True:
-        game_over,pontuacao = game.playloop()
-        if game_over == True:
-            break
-    print('Pontuação final: ', pontuacao)
